b34a029b261471afbc08714ec209a004e2f9d03b
Vinícios Barbosa <viniciosbarbosa740@gmail.com>
2024-02-24 22:30:00 -0300

    refactor: :recycle: update imports, layout, and sections

---
+++ data/contact.ts
@@ -0,0 +1,14 @@
+export const contactLinks = [
+  {
+    label: 'viniciosbarbosa.dev',
+    url: 'https://viniciosbarbosa.dev',
+  },
+  {
+    label: 'viniciosbarbosa.dev@gmail.com',
+    url: 'mailto:viniciosbarbosa.dev@gmail.com',
+  },
+  {
+    label: 'linkedin.com/in/vinebarbosabarbosa',
+    url: 'https://linkedin.com/in/vinebarbosabarbosa',
+  },
+]
